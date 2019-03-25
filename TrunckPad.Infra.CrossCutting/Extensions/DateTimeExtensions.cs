d1747ee821bad4d94ea0e28f5894f1e152135835
ValeskBelice <marcosvbelice@gmail.com>
2019-03-25 15:10:27 -0300

    Add files via upload

---
+++ TrunckPad.Infra.CrossCutting/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TrunckPad.Infra.CrossCutting.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string Formatado(this DateTime strIn, string masc)
+        {
+            var retorno = string.Format(CultureInfo.GetCultureInfo("pt-BR"), masc, strIn);
+            return retorno;
+        }
+
+        public static string Formatado(this DateTime? strIn, string masc)
+        {
+            string retorno = "";
+            if (strIn != null)
+            {
+                return retorno = string.Format(CultureInfo.GetCultureInfo("pt-BR"), masc, strIn);
+            }
+            return retorno;
+        }
+    }
+}
